34a7b23fc53a04187126ceee93b3818c9394cee6
mirasha kavindi <111460128+mirasha0@users.noreply.github.com>
2023-12-20 10:11:27 +0530

    new

--- notify_url.php
+++ notify_url.php
@@ -1,13 +1,15 @@
 <?php
 
-$merchant_id         = $_POST['1225212'];
-$order_id            = $_POST['ItemNo12346'];
-$payhere_amount      = $_POST['30.00'];
-$payhere_currency    = $_POST['LKR'];
-$status_code         = $_POST['2'];
-$md5sig              = $_POST['local_md5sig'];
+error_log('Received POST data: ' . print_r($_POST, true));
 
-$merchant_secret = 'MjQ0MzY3NDU3NTM1Njg0MDA0NzQyNzU1ODQxOTY0NDEwNDcwMzAzMg=='; // Replace with your Merchant Secret
+$merchant_id         = isset($_POST['merchant_id']) ? $_POST['merchant_id'] : '';
+$order_id            = isset($_POST['order_id']) ? $_POST['order_id'] : '';
+$payhere_amount      = isset($_POST['amount']) ? $_POST['amount'] : '';
+$payhere_currency    = isset($_POST['currency']) ? $_POST['currency'] : '';
+$status_code         = isset($_POST['status_code']) ? $_POST['status_code'] : '';
+$md5sig              = isset($_POST['md5sig']) ? $_POST['md5sig'] : '';
+
+$merchant_secret = 'MjQ0MzY3NDU3NTM1Njg0MDA0NzQyNzU1ODQxOTY0NDEwNDcwMzAzMg==';
 
 $local_md5sig = strtoupper(
     md5(
@@ -16,19 +18,20 @@ $local_md5sig = strtoupper(
         $payhere_amount . 
         $payhere_currency . 
         $status_code . 
-        strtoupper(md5($merchant_secret)) 
+        $merchant_secret 
     ) 
 );
-       
-if (($local_md5sig === $md5sig) AND ($status_code == 2) ){
+
+error_log('Local MD5 Signature: ' . $local_md5sig);
+error_log('Received MD5 Signature: ' . $md5sig);
+
+if (($local_md5sig === $md5sig) AND ($status_code == '2')) {
     error_log('Successful payment for order ID: ' . $order_id);
     error_log('POST data received: ' . print_r($_POST, true));
-
 } else {
     // Log an error or take appropriate action for failed validation
     error_log('Payment validation failed for order ID: ' . $order_id);
-    error_log('POST data received:12 ' . print_r($_POST, true));
-
+    error_log('POST data received: ' . print_r($_POST, true));
 }
 
 ?>
