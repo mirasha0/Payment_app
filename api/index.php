02ce72d7b2c8d832ab4c844dc3ad5df6f5c9f7e0
mirasha kavindi <111460128+mirasha0@users.noreply.github.com>
2023-12-27 10:26:45 +0530

    latest

--- api/index.php
+++ api/index.php
@@ -1,11 +1,11 @@
 <?php
 
-$merchant_id         = isset($_POST['1225212']) ? $_POST['merchant_id'] : '';
-$order_id            = isset($_POST['ItemNo12346']) ? $_POST['order_id'] : '';
-$payhere_amount      = isset($_POST['30.00']) ? $_POST['amount'] : '';
-$payhere_currency    = isset($_POST['LKR']) ? $_POST['currency'] : '';
-$status_code         = isset($_POST['2']) ? $_POST['status_code'] : '';
-$md5sig              = isset($_POST['local_md5sig']) ? $_POST['md5sig'] : '';
+$merchant_id         = $_POST['merchant_id'];
+$order_id            = $_POST['order_id'];
+$payhere_amount      = $_POST['payhere_amount'];
+$payhere_currency    = $_POST['payhere_currency'];
+$status_code         = $_POST['status_code'];
+$md5sig              = $_POST['md5sig'];
 
 $merchant_secret = 'MzE4MjcwNTIzMDI1OTkwNDkwMjExMDUyOTQ1ODQzMjk3MzQ0MDE5Mg=='; // Replace with your Merchant Secret
 
@@ -21,7 +21,7 @@ $local_md5sig = strtoupper(
 );
        
 if (empty($_POST)) {
-    error_log('No POST data received...........');
+    error_log('No POST data received.');
 } elseif (($local_md5sig === $md5sig) AND ($status_code == '2')) {
     echo('Successful payment for order ID: ' . $order_id);
     echo('POST data received: ' . print_r($_POST, true));
@@ -29,7 +29,10 @@ if (empty($_POST)) {
     // Log an error or take appropriate action for failed validation
     echo('Payment validation failed for order ID: ' . $order_id);
     echo('POST data received: ' . print_r($_POST, true));
-
 }
+
+/*if (($local_md5sig === $md5sig) AND ($status_code == 2) ){
+    //TODO: Update your database as payment success
+}*/
 
 ?>
