e7639fab8e5f92a3a0ad50287460e8bed1453fdf
mirasha kavindi <111460128+mirasha0@users.noreply.github.com>
2023-12-27 10:18:14 +0530

    new

--- api/index.php
+++ api/index.php
@@ -1,5 +1,4 @@
 <?php
-echo "Hello World!";
 
 $merchant_id         = $_POST['merchant_id'];
 $order_id            = $_POST['order_id'];
@@ -23,7 +22,7 @@ $local_md5sig = strtoupper(
        
 if (($local_md5sig === $md5sig) AND ($status_code == 2) ){
         //TODO: Update your database as payment success
-        echo "Hello World!123";
+        
         
 }
 
